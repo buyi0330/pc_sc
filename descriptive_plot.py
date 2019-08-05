ab6d427c0a818c06454b518ca70f9965cb4f9a7b
Yi Bu <buyipku@gmail.com>
2019-08-05 00:30:47 -0500

    yyy

--- descriptive_plot.py
+++ descriptive_plot.py
@@ -42,7 +42,7 @@ def scatter_pdf (a_list, path_name, file_name, event_name):
     plt.xscale("log")
     plt.yscale("log")
     
-    plt.subplots_adjust (left = 2, right = 4, top = 4, bottom = 2, wspace = 0.5, hspace = 0.5)
+    #plt.subplots_adjust (left = 2, right = 4, top = 4, bottom = 2, wspace = 0.5, hspace = 0.5)
     plt.savefig(str(path_name) + "/" + str(file_name) + ".jpg", dpi = 600)
     plt.close()
     
