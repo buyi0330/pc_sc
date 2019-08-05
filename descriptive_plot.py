a574fc4dd3721757e00c21d80a44b78fc95b5852
Yi Bu <buyipku@gmail.com>
2019-08-05 00:11:30 -0500

    yyy

--- descriptive_plot.py
+++ descriptive_plot.py
@@ -44,6 +44,7 @@ def scatter_pdf (a_list, path_name, file_name, event_name):
     
     plt.subplots_adjust (left = 2, right = 4, top = 4, bottom = 2, wspace = 0.5, hspace = 0.5)
     plt.savefig(str(path_name) + "/" + str(file_name) + ".jpg", dpi = 600)
+    plt.close()
     
 # Given a dictionary, return x_list as its key list, and y_list as its value list (correspondingly)
 def dic_plot (dic):
@@ -98,9 +99,9 @@ for line in inFile:
     count += 1
 
 scatter_pdf(repo_pushCount, "/kellogg/proj/ybo1623", "push distribution", "pushes")
-scatter_pdf(repo_pullCount, "/kellogg/proj/ybo1623", "pull distribution", "pulls")
-scatter_pdf(repo_forkCount, "/kellogg/proj/ybo1623", "fork distribution", "forks")
-scatter_pdf(repo_userCount, "/kellogg/proj/ybo1623", "user distribution", "users")
+#scatter_pdf(repo_pullCount, "/kellogg/proj/ybo1623", "pull distribution", "pulls")
+#scatter_pdf(repo_forkCount, "/kellogg/proj/ybo1623", "fork distribution", "forks")
+#scatter_pdf(repo_userCount, "/kellogg/proj/ybo1623", "user distribution", "users")
 '''
 plt.plot(repo_userCount, repo_pushCount, "r+", markersize = 2)
 plt.xlabel("number of users in a repo")
